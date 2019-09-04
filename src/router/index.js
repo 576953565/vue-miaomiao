ed61d6ba3818680b910d5361a28de4634b1f1b13
wubaiwan <576953565@qq.com>
2019-09-04 21:21:42 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+//单独写路由文件引入
 import movieRouter from './movie'
 import mineRouter from './mine'
 import cinema from './cinema'
+//引入组件，然后在写路由
+import movie from '@/views/movie'
 
 Vue.use(Router)
 
 export default new Router({
   mode:'history',
   routes:[
-    movieRouter,
-    mineRouter,
-    cinema
+    {
+      path:'/movie',
+      component:movie
+    },
+    {
+      path:'/mine',
+      component:()=>import('@/views/mine')  //写路由的同时引入组件
+    },
+    {
+      path:'/cinema',
+      component:()=>import('@/views/cinema')
+    }
+    // movieRouter,
+    // mineRouter,
+    // cinema
   ]
 })
